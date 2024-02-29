a3586727c6e00cc1639b898b0bd2f87a8bc047a2
TsuyosiUsugi <qesulive@gmail.com>
2024-02-29 22:20:32 +0900

    [update] インゲーム開始からランゲームスタートまでの処理がとりあえずできた

--- Assets/SoulRunProject/Program/Scripts/InGame/Core/LifeSpan/InGameLifeTimeManager.cs
+++ Assets/SoulRunProject/Program/Scripts/InGame/Core/LifeSpan/InGameLifeTimeManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using SoulRunProject.InGameTest;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -12,9 +13,16 @@ namespace SoulRunProject.Common
     public class InGameLifeTimeManager : LifetimeScope
     {
         [SerializeField] private TestCamera _camera;
+        [SerializeField] private PlayerMovement _playerMovement;
+        [SerializeField] private TestPlayerForwardMover _testPlayerForwardMover;
         protected override void Configure(IContainerBuilder builder)
         {
+            //ドメイン層
             builder.RegisterInstance(_camera);
+            builder.RegisterInstance(_playerMovement);
+            builder.RegisterInstance(_testPlayerForwardMover);
+            
+            //アプリケーション層
             builder.Register<State, EnterInGameState>(Lifetime.Singleton);
             builder.Register<EnterStageState>(Lifetime.Singleton);
             builder.Register<PlayingRunGameState>(Lifetime.Singleton);
