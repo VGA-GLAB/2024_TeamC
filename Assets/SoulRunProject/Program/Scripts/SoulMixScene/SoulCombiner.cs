3c88a0192b1348ca4f9bfedd0e36d45d3c4ea733
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-03-06 16:55:58 +0900

    [Update]

--- Assets/SoulRunProject/Program/Scripts/SoulMixScene/SoulCombiner.cs
+++ Assets/SoulRunProject/Program/Scripts/SoulMixScene/SoulCombiner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using SoulRun.InGame;
 using UnityEngine;
 using UnityEngine.Events;
@@ -7,49 +8,45 @@ using UnityEngine.EventSystems;
 
 namespace SoulRunProject.SoulMixScene
 {
-    /// <summary> 組み合わせを判定するクラス </summary>
+    /// <summary> ソウルを合成するクラス </summary>
     public class SoulCombiner : MonoBehaviour
     {
-        public SoulCardList ownedSouls; // 所持しているソウルのリスト
-        public List<SoulCombination> combinations; // 使用可能な配合表のリスト
+        public SoulCardList ownedSouls; // 所持しているソウルリスト
+        public List<SoulCombination> combinations; // ソウルの組み合わせリスト
 
-        public InputUIButton soul1Button; // ソウル1のボタン
+        public InputUIButton soul1Button;
 
         private void Awake()
         {
-            TryGetComponent(out soul1Button);
+            TryGetComponent(out soul1Button); // この使用例はもしsoul1Buttonが自動的に設定されるべきなら意味があるが、明確化が必要
             soul1Button.OnClick.AddListener(SelectSoul1);
         }
 
-        // ソウル1を選択するメソッド
         private void SelectSoul1(InputUIButton button)
         {
             Debug.Log("SelectSoul1");
         }
+        
+        
 
-        // 2つのソウルを組み合わせて新しいソウルを作成するメソッド
-        public SoulCard Combine(SoulCard soul1, SoulCard soul2)
+        /// <summary> ソウルを合成する </summary>
+        public SoulCard Combine(SoulCard selectSoul1, SoulCard selectSoul2)
         {
-            foreach (var combination in combinations)
-            {
-                if (combination.IsValidCombination(soul1, soul2))
-                {
-                    // ソウルカードのリストから削除
-                    ownedSouls.soulCardList.Remove(soul1);
-                    ownedSouls.soulCardList.Remove(soul2);
+            if (!combinations.Any(combination =>
+                    combination.IsValidCombination(selectSoul1, selectSoul2))) return null;
 
-                    // 新しいソウルカードの作成
-                    // ここで、combination.result は新しいソウルカードのプロトタイプを指すと仮定しています。
-                    // 新しいインスタンスを作成する方法は、プロジェクトの設計によります。
-                    SoulCard newSoul = Instantiate(combination.result);
+            ownedSouls.soulCardList.Remove(selectSoul1);
+            ownedSouls.soulCardList.Remove(selectSoul2);
 
-                    // 新しいソウルカードをリストに追加
-                    ownedSouls.soulCardList.Add(newSoul);
-                    return newSoul; // 新しいソウルカードを返す
-                }
-            }
+            // 何を作るかを決定する
+            SoulCard newSoul = ScriptableObject.CreateInstance<SoulCard>();
 
-            return null; // 有効な組み合わせがない場合はnullを返す
+            // 作ったソウルにDataを設定する
+
+
+            // 作ったソウルをリストに追加する
+            ownedSouls.soulCardList.Add(newSoul);
+            return newSoul;
         }
     }
 }
