d50f3359db3df68ad1fe2eef1ef30d88e8323408
TsuyosiUsugi <qesulive@gmail.com>
2024-03-06 10:26:56 +0900

    [update] ボスの移動処理追加

--- Assets/Member/Usugi/BossTest/SnakeBody.cs
+++ Assets/Member/Usugi/BossTest/SnakeBody.cs
@@ -2,20 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SoulRunProject
+namespace SoulRunProject.InGame
 {
     public class SnakeBody : MonoBehaviour
     {
         public Transform target;
         public float smoothSpeed = 0.125f;
         public Vector3 offset;
-        private List<Vector3> positions;
+        private Queue<Vector3> positions = new();
         public float stopDistance = 0.1f; // 動きを停止する距離の閾値
-
-        void Start()
-        {
-            positions = new List<Vector3>();
-        }
 
         void Update()
         {
@@ -23,13 +18,11 @@ namespace SoulRunProject
             if (Vector3.Distance(transform.position, target.position) > stopDistance)
             {
                 // 前の位置を記録
-                positions.Insert(0, target.position);
-
-                // 体の部分を移動させる
+                positions.Enqueue(target.position);
+                
                 if (positions.Count > 10) // 10は体の長さに応じて調整してください
                 {
-                    transform.position = Vector3.Lerp(transform.position, positions[positions.Count - 1] + offset, smoothSpeed);
-                    positions.RemoveAt(positions.Count - 1);
+                    transform.position = Vector3.Lerp(transform.position, positions.Dequeue() + offset, smoothSpeed);
                 }
             }
         }
