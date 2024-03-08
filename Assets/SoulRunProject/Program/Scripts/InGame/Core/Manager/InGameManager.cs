605a88044b39f52d67e53aed2c08ea459286cca6
TsuyosiUsugi <qesulive@gmail.com>
2024-03-08 16:26:45 +0900

    [fix] 不要なUsing削除

--- Assets/SoulRunProject/Program/Scripts/InGame/Core/Manager/InGameManager.cs
+++ Assets/SoulRunProject/Program/Scripts/InGame/Core/Manager/InGameManager.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
-using SoulRunProject.InGame;
 using UnityEngine;
-using VContainer;
 using VContainer.Unity;
 
 namespace SoulRunProject.Common
