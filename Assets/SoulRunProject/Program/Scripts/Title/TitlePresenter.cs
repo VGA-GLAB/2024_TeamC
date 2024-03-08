617f384712ddfc10a3082daa7f69f1ea5b6ade6b
TsuyosiUsugi <qesulive@gmail.com>
2024-03-08 14:10:48 +0900

    [add] タイトル用のスクリプト追加、デバッククラスの名前空間変更

---
+++ Assets/SoulRunProject/Program/Scripts/Title/TitlePresenter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoulRunProject
+{
+    public class TitlePresenter : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/SoulRunProject/Program/Scripts/Title/TitleView.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoulRunProject.Title
+{
+    /// <summary>
+    /// タイトルの表示処理を行うクラス
+    /// </summary>
+    public class TitleView : MonoBehaviour
+    {
+        [SerializeField] private Button _startButton;
+        [SerializeField] private Button _optionButton;
+        [SerializeField] private Button _exitButton;
+    }
+}
