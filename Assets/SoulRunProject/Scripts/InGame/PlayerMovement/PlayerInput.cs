714f17bfedf0a126c9a44bf4d7cad94dc8720d19
showgo-koinuma <van71413989i@gmail.com>
2024-03-20 18:15:37 +0900

    [add] PauseStateの追加、切替、PlayerManagerのSwitchPauseの切替

--- Assets/SoulRunProject/Scripts/InGame/PlayerMovement/PlayerInput.cs
+++ Assets/SoulRunProject/Scripts/InGame/PlayerMovement/PlayerInput.cs
@@ -1,3 +1,4 @@
+using System;
 using UniRx;
 using UnityEngine;
 
@@ -10,20 +11,24 @@ namespace SoulRunProject.InGame
     {
         private readonly FloatReactiveProperty _horizontalInput = new FloatReactiveProperty();
         private readonly BoolReactiveProperty _jumpInput = new BoolReactiveProperty();
+        private readonly BoolReactiveProperty _pauseInput = new BoolReactiveProperty();
 
         public FloatReactiveProperty HorizontalInput => _horizontalInput;
         public BoolReactiveProperty JumpInput => _jumpInput;
+        public BoolReactiveProperty PauseInput => _pauseInput;
 
         private void Awake()
         {
             _horizontalInput.AddTo(this);
             _jumpInput.AddTo(this);
+            _pauseInput.AddTo(this);
         }
 
         private void Update()
         {
             _horizontalInput.Value = Input.GetAxisRaw("Horizontal");
             _jumpInput.Value = Input.GetButtonDown("Jump");
+            _pauseInput.Value = Input.GetButtonDown("Cancel");
         }
     }
 }
