2860b65118feb0caac184a7ef28a95b66049c260
TsuyosiUsugi <qesulive@gmail.com>
2024-03-22 18:46:37 +0900

    [update] 上げ忘れ

--- Assets/SoulRunProject/Scripts/InGame/SoulSkill/TestSoul.cs
+++ Assets/SoulRunProject/Scripts/InGame/SoulSkill/TestSoul.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace SoulRunProject.InGame
 {
+    [Serializable]
     public class TestSoul : SoulSkillBase
     {
         protected override void StartSoulSkill()
